8ede3a688f8324a0ccca3ad6b31d65516368fc8d
mohamed hadaey <105833278+MohamedHadaey@users.noreply.github.com>
2023-02-14 00:50:33 +0200

    final upload of tapy profile design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+var exchangeContact = document.getElementById("exchange-contact");
+var backBtn = document.getElementById("backBtn");
+var contactContainer = document.getElementById("contact-container");
+var parentContainer = document.getElementById("parent-container");
+
+exchangeContact.onclick = function () {
+  contactContainer.style.display = "block";
+  parentContainer.style.display = "none"
+}
+
+backBtn.onclick = function () {
+  contactContainer.style.display = "none";
+  parentContainer.style.display = "block"
+}
+
+
+/***************************************/
+var timeInput = document.getElementById("time");
+var hour = "06:00";
+var hour = document.querySelectorAll("#hour");
+
+for (var i=0 ; i<hour.length ; i++){
+
+  hour[i].addEventListener("click",function(e){
+
+    for (var i=0 ; i<hour.length ; i++){ 
+      hour[i].classList.remove("active");
+    }
+
+      var element = e.target;
+      element.classList.add('active');
+
+      hour.value = element.innerText;
+      timeInput.value = hour.value;
+  })
+}
+
+
 /*******       All swiper      *********/
 var swiper = new Swiper(".myAllSwiper", {
     speed: 1500,
@@ -79,7 +117,134 @@ var swiper = new Swiper(".myFacilitiesSwiper", {
     },
   });
 
-
-
+// end of:: wiper slides 
+var calendarInfoDate = document.querySelector('#vanilla-calendar-info-date');
+var calendarInfoTime = document.querySelector('#vanilla-calendar-info-time');
+var username = document.getElementById("username");
+var phone = document.getElementById("phone");
+var email = document.getElementById("email");
+var submitBtn = document.getElementById("submitBtn");
 
   
+submitBtn.onclick = function () { 
+  var appointment = {
+    date: calendarInfoDate.innerText,
+    time: timeInput.value,
+    username: username.value,
+    phone: phone.value,
+    email: email.value,
+  };
+  console.log(appointment);
+  clearForm();
+}
+
+
+/************      Clear Data       ********************/
+var inputs = document.getElementsByClassName("form-control");
+function clearForm() {
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].value = "";
+  }
+}
+
+// sweet calender
+document.addEventListener('DOMContentLoaded', () => {
+  const calendarInfoDate = document.querySelector('#vanilla-calendar-info-date');
+  const calendarInfoTime = document.querySelector('#vanilla-calendar-info-time');
+  const calendar = new VanillaCalendar('#vanilla-calendar', {
+    settings: {
+      lang: 'en',
+      iso8601: false,
+      selection: {
+        time: 12,
+      },
+      visibility: {
+        weekNumbers: true,
+      },
+    },
+    actions: {
+      clickDay(e, dates) {
+        calendarInfoDate.innerText = dates[0] ? dates : '«Day not selected»';
+      },
+      clickWeekNumber(e, number, days, year) {
+        console.log(`Week number: ${number}, year of the week: ${year},`, 'Days of this week:', days);
+      },
+      clickMonth(e, month) {
+        console.log(`Current month: ${month}`);
+      },
+      clickYear(e, year) {
+        console.log(`Current year: ${year}`);
+      },
+      clickArrow(e, year, month) {
+        console.log(`Current year: ${year}, current month: ${month}`);
+      },
+      changeTime(e, time) {
+        calendarInfoTime.innerText = time;
+      },
+    },
+    popups: {
+      '2022-12-28': {
+        modifier: 'bg-red',
+        html: 'Meeting at 9:00 PM',
+      },
+      '2022-09-13': {
+        modifier: 'bg-red',
+        html: 'Meeting at 6:00 PM',
+      },
+      '2022-11-17': {
+        modifier: 'bg-orange s',
+        html: `<div>
+          <u><b>12:00 PM</b></u>
+          <p style="margin: 5px 0 0; text-align: left; font-size: 12px;">Airplane in Las Vegas</p>
+        </div>`,
+      },
+    },
+    DOMTemplates: {
+      default: `
+        <div class="vanilla-calendar-header">
+          <div class="vanilla-calendar-header__content ">
+            <#Month /> | <#Year />
+          </div>
+          <#ArrowPrev />
+          <#ArrowNext />
+        </div>
+        <div class="vanilla-calendar-wrapper">
+          <#WeekNumbers />
+          <div class="vanilla-calendar-content">
+            <#Week />
+            <#Days />
+          </div>
+        </div>
+        <#ControlTime />
+      `,
+      month: `
+        <div class="vanilla-calendar-header">
+          <div class="vanilla-calendar-header__content">
+            <#Month /> | <#Year />
+          </div>
+        </div>
+        <div class="vanilla-calendar-wrapper">
+          <div class="vanilla-calendar-content">
+            <#Months />
+          </div>
+        </div>
+      `,
+      year: `
+        <div class="vanilla-calendar-header">
+          <div class="vanilla-calendar-header__content">
+            <#Month /> | <#Year />
+          </div>
+          <#ArrowPrev />
+          <#ArrowNext />
+        </div>
+        <div class="vanilla-calendar-wrapper">
+          <div class="vanilla-calendar-content">
+            <#Years />
+          </div>
+        </div>
+      `,
+    },
+  });
+  calendar.init();
+  calendarInfoTime.innerText = calendar.selectedTime;
+});
